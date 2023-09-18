fd76e86cefa4d0307cb17be45a0a5da4549488ec
andres.mera <feli041089@gmail.com>
2023-09-18 13:50:37 -0500

    Como identificar las secciones en FormKit y aplicar css personalizando.

--- formkit.config.js
+++ formkit.config.js
@@ -1,5 +1,16 @@
+import { generateClasses } from "@formkit/themes";
+
 const config = {
-  config: {},
+  config: {
+    classes: generateClasses({
+      text: {
+        message: "text-red-500",
+      },
+      number: {
+        message: "text-red-500",
+      },
+    }),
+  },
 };
 
 export default config;
