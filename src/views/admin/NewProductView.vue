5f6732552ca06d69b640a292e12cbe4c97ca78b9
andres.mera <feli041089@gmail.com>
2023-09-18 10:47:11 -0500

    Añadiendo los campos a Nuevo Producto

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -7,9 +7,26 @@ import Link from '../../components/Link.vue';
         <Link to="products">Volver</Link>
         <h1 class="text-4xl font-black my-10">Nuevo Producto</h1>
 
-        <FormKit type="form">
-            <FormKit type="text" />
-        </FormKit>
+        <div class="flex justify-center bg-white shadow">
+            <div class="mt-10 p-10 w-full 2xl:w-2/4">
+
+                <FormKit type="form" submit-label="Agregar Producto">
+                    <FormKit type="text" label="Nombre" name="name" placeholder="Nombre del Producto" validation="required"
+                        :validation-messages="{ required: 'El Nombre del Producto es Obligatorio' }" />
+                    <FormKit type="file" label="Imgen Producto" name="image" validation="required"
+                        :validation-messages="{ required: 'La Imagen del Producto es Obligatoria' }" accept=".jpg"
+                        multiple="false" />
+                    <FormKit type="select" label="Categoría" name="category" validation="required"
+                        :validation-messages="{ required: 'La Categoría es Obligatoria' }" :options="[1, 2, 3]" />
+                    <FormKit type="number" label="Precio" name="price" placeholder="Precio del Producto"
+                        validation="required" :validation-messages="{ required: 'El Precio del Producto es Obligatorio' }"
+                        min="1" step="10" />
+                    <FormKit type="number" label="Disponibles" name="availability"
+                        placeholder="Cantidad disponible del Producto" validation="required"
+                        :validation-messages="{required:'La Cantidad del Producto es Obligatorio'}" min="1" step="10" />
+                </FormKit>
+
+        </div>
     </div>
-</template>
+</div></template>
 
