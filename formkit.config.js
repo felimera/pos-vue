5bbca595797807faf93b0eb7531f41a21101b025
andres.mera <feli041089@gmail.com>
2023-09-18 14:07:36 -0500

    CSS a los inputs

--- formkit.config.js
+++ formkit.config.js
@@ -7,6 +7,12 @@ const config = {
         label: "block mb-1 font-bold text-lg",
         message: "text-red-500 mb-5",
         wrapper: "space-y-2 mb-3",
+        input:
+          "w-full p-3 border border-gray-300 rounded text-gray-700 placeholder-gray-400",
+      },
+      file: {
+        noFiles: "block my-2",
+        fileItem: "hidden",
       },
     }),
   },
