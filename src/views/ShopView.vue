882fc1a862954607c094a9e9dc6209c18ffd6f9f
andres.mera <feli041089@gmail.com>
2023-09-18 08:13:44 -0500

    Creando la barra principal

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,8 +1,10 @@
-<script setup></script>
+<script setup>
+import MainNav from '../components/MainNav.vue';
+</script>
 
 <template>
     <div>
-        <h1>Shop View</h1>
+        <MainNav />
     </div>
 </template>
 
