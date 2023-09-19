b133b47349f0e0bc15652dcd032b00edb0b6aaef
andres.mera <feli041089@gmail.com>
2023-09-19 13:24:57 -0500

    Primeros pasos con la Edicción

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { formatCurrency } from '../helpers'
+import { RouterLink } from 'vue-router';
+import { formatCurrency } from '../helpers';
 
 defineProps({
     product: {
@@ -11,10 +12,23 @@ defineProps({
 <template>
     <li class="flex items-center space-x-6 border border-gray-200 p-6 bg-white shadow">
         <img :src="product.image" :alt="product.name" class="h-24 w-24">
-        <div class="space-y-2">
+        <div class="space-y-2 flex-auto">
             <h3 class="text-gray-900">{{ product.name }}</h3>
             <p class="font-extrabold">{{ formatCurrency(product.price) }}</p>
             <p>{{ product.availability }} en Existencia</p>
+        </div>
+        <div>
+            <RouterLink :to="{
+                name: 'edit-product',
+                params: { id: product.id }
+            }">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 text-gray-600">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                </svg>
+            </RouterLink>
+
         </div>
     </li>
 </template>
