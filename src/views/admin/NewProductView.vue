e9677c97b8f66482c45563903b803caa7485ab6f
andres.mera <feli041089@gmail.com>
2023-09-19 08:37:44 -0500

    Mostrando las Categorias en el Formulario

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,8 +1,23 @@
 <script setup>
+import { reactive } from 'vue';
 import Link from '../../components/Link.vue';
 import useImge from '../../composables/useImge'
+import { useProductsStore } from '../../stores/products'
 
 const { url, onFileChange, isImageUploaded } = useImge();
+const products = useProductsStore();
+
+const formData = reactive({
+    name: '',
+    category: '',
+    price: '',
+    availability: '',
+    image: ''
+});
+
+const submitHandler = data => {
+    console.log('data', data)
+}
 
 </script>
 
@@ -15,30 +30,33 @@ const { url, onFileChange, isImageUploaded } = useImge();
             <div class="mt-10 p-10 w-full 2xl:w-2/4">
 
                 <FormKit type="form" submit-label="Agregar Producto"
-                    incomplete-message="Campos incompletos: Debes agregar los campos obligatorios.">
+                    incomplete-message="Campos incompletos: Debes agregar los campos obligatorios." @submit="submitHandler"
+                    :value="formData">
                     <FormKit type="text" label="Nombre" name="name" placeholder="Nombre del Producto" validation="required"
-                        :validation-messages="{ required: 'El Nombre del Producto es Obligatorio' }" />
+                        :validation-messages="{ required: 'El Nombre del Producto es Obligatorio' }"
+                        v-model.trim="formData.name" />
                     <FormKit type="file" label="Imgen Producto" name="image" validation="required"
                         :validation-messages="{ required: 'La Imagen del Producto es Obligatoria' }" accept=".jpg"
-                        multiple="false" @change="onFileChange" />
+                        multiple="false" @change="onFileChange" v-model.trim="formData.image" />
 
                     <div v-if="isImageUploaded">
                         <p class="font-black">Imagen Producto:</p>
                         <img :src="url" alt="Nueva imagen producto" class="w-32" />
                     </div>
-                    
+
                     <FormKit type="select" label="Categoría" name="category" validation="required"
-                        :validation-messages="{ required: 'La Categoría es Obligatoria' }" :options="[1, 2, 3]" />
+                        :validation-messages="{ required: 'La Categoría es Obligatoria' }" :options="[1, 2, 3]"
+                        v-model.number="formData.category" />
                     <FormKit type="number" label="Precio" name="price" placeholder="Precio del Producto"
                         validation="required" :validation-messages="{ required: 'El Precio del Producto es Obligatorio' }"
-                        min="1" step="10" />
-                    <FormKit type="number" label="Disponibles" name="availability"
-                        placeholder="Cantidad disponible del Producto" validation="required"
-                        :validation-messages="{ required: 'La Cantidad del Producto es Obligatorio' }" min="1" step="10" />
-                </FormKit>
+                        min="1" v-model.number="formData.price" />
+                <FormKit type="number" label="Disponibles" name="availability"
+                    placeholder="Cantidad disponible del Producto" validation="required"
+                    :validation-messages="{ required: 'La Cantidad del Producto es Obligatorio' }" min="1"
+                    v-model.number="formData.availability" />
+            </FormKit>
 
-            </div>
         </div>
     </div>
-</template>
+</div></template>
 
