bca5bc52624c8ff2896f6a802b94fdeb49702242
andres.mera <feli041089@gmail.com>
2023-09-20 14:26:17 -0500

    Permitir solo agregar un maximo de 5 o los articulos que haya disponibles

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -4,15 +4,23 @@ import { defineStore } from "pinia";
 export const useCartStore = defineStore("cart", () => {
   const items = ref([]);
 
+  const MAX_PRODUCTS = 5;
+
   function addItem(item) {
     items.value.push(item);
   }
 
   const isEmpty = computed(() => items.value.length === 0);
 
+  const checkProductAvailability = computed(
+    () => (product) =>
+      product.availability < MAX_PRODUCTS ? product.availability : MAX_PRODUCTS
+  );
+
   return {
     items,
     addItem,
     isEmpty,
+    checkProductAvailability,
   };
 });
