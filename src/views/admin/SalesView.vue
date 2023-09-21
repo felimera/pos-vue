c68a53012488f71d1ad0e77deb25b24ed9f2e6aa
andres.mera <feli041089@gmail.com>
2023-09-21 14:43:53 -0500

    Personalizar el Calendario

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { ref } from 'vue';
 import VueTailwindDatePicker from 'vue-tailwind-datepicker'
+
+const date = ref('');
+const formatter = ref({
+    date: 'DD/MM/YYYY',
+    month: 'MMMM'
+});
+
 </script>
 
 <template>
     <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
     <div class="md:flex md:items-start gap-5">
-        <div class="md:w-1/2 lg:w-1/3 bg-white flex justify-center">
-            <VueTailwindDatePicker />
+        <div class="md:w-1/2 lg:w-1/3 bg-white flex justify-center p-5">
+            <VueTailwindDatePicker i18n="es-es" as-single="true" no-input v-model="date" :formatter="formatter" />
         </div>
         <div class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-scroll p-5 pb-32">
             <p>Ventas aquí</p>
