038ea1f71f79bf5ae445dc8889beca11adc958a7
andres.mera <feli041089@gmail.com>
2023-09-21 07:55:32 -0500

    Creando un componente para Mostrar las Cantidades

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,6 +1,8 @@
 <script setup>
+import Amount from './Amount.vue';
 import ShopppingCartItem from './ShopppingCartItem.vue'
 import { useCartStore } from '../stores/cart';
+import { formatCurrency } from '../helpers';
 
 const cart = useCartStore();
 </script>
@@ -13,5 +15,20 @@ const cart = useCartStore();
         <ul role="list" class="mt-16 divide-y divide-gray-300">
             <ShopppingCartItem v-for="item in cart.items" :key="item.id" :item="item" />
         </ul>
+
+        <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
+            <Amount>
+                <template #label>Subtotal : </template>
+                {{ formatCurrency(item.price) }}
+            </Amount>
+            <Amount>
+                <template #label>Impuestos : </template>
+                {{ formatCurrency(0) }}
+            </Amount>
+            <Amount>
+                <template #label>Total a Pagar : </template>
+                {{ formatCurrency(0) }}
+            </Amount>
+        </dl>
     </div>
 </template>
