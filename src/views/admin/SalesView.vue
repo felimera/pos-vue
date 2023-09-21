57d04d48585e7a4afa9b5566165c4215cf6dd7b0
andres.mera <feli041089@gmail.com>
2023-09-21 14:34:31 -0500

    Configuracion del plugin de calendario para filtrar las ventas.

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -1,8 +1,16 @@
-<script setup></script>
+<script setup>
+import VueTailwindDatePicker from 'vue-tailwind-datepicker'
+</script>
 
 <template>
-    <div>
-        <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
+    <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
+    <div class="md:flex md:items-start gap-5">
+        <div class="md:w-1/2 lg:w-1/3 bg-white flex justify-center">
+            <VueTailwindDatePicker />
+        </div>
+        <div class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-scroll p-5 pb-32">
+            <p>Ventas aquí</p>
+        </div>
     </div>
 </template>
 
