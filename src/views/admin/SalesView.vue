a17f6effd68f6f2b793eee802b4062e1d840e76c
andres.mera <feli041089@gmail.com>
2023-09-21 14:59:04 -0500

    Validar si el usuario selecciono una fecha

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { ref } from 'vue';
-import VueTailwindDatePicker from 'vue-tailwind-datepicker'
+import VueTailwindDatePicker from 'vue-tailwind-datepicker';
+import { useSalesStore } from '../../stores/sales';
 
-const date = ref('');
+const sales = useSalesStore();
+
 const formatter = ref({
     date: 'DD/MM/YYYY',
     month: 'MMMM'
@@ -14,10 +16,12 @@ const formatter = ref({
     <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
     <div class="md:flex md:items-start gap-5">
         <div class="md:w-1/2 lg:w-1/3 bg-white flex justify-center p-5">
-            <VueTailwindDatePicker i18n="es-es" as-single="true" no-input v-model="date" :formatter="formatter" />
+            <VueTailwindDatePicker i18n="es-es" as-single="true" no-input v-model="sales.date" :formatter="formatter" />
         </div>
         <div class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-scroll p-5 pb-32">
-            <p>Ventas aquí</p>
+            <p v-if="sales.isDateSelected" class="text-center text-lg">Ventas de la fecha: <span class="font-black">{{
+                sales.date }}</span></p>
+            <p v-else class="text-center text-lg">Seleccione una Fecha</p>
         </div>
     </div>
 </template>
