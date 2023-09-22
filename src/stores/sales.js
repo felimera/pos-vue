c689612e4706c727c409042329ed05736ae37956
andres.mera <feli041089@gmail.com>
2023-09-22 07:26:22 -0500

    Consultar las ventas por fecha

--- src/stores/sales.js
+++ src/stores/sales.js
@@ -1,13 +1,26 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import { collection, query, where } from "firebase/firestore";
+import { useFirestore, useCollection } from "vuefire";
 
 export const useSalesStore = defineStore("sales", () => {
   const date = ref("");
+  const db = useFirestore();
+
+  const salesSource = computed(() => {
+    if (date.value) {
+      const q = query(collection(db, "sales"), where("date", "==", date.value));
+      return q;
+    }
+  });
+
+  const salesCollection = useCollection(salesSource);
 
   const isDateSelected = computed(() => date.value);
 
   return {
     date,
     isDateSelected,
+    salesCollection,
   };
 });
--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import VueTailwindDatePicker from 'vue-tailwind-datepicker';
 import { useSalesStore } from '../../stores/sales';
+import SaleDetails from '../../components/SaleDetails.vue';
 
 const sales = useSalesStore();
 
@@ -16,12 +17,16 @@ const formatter = ref({
     <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
     <div class="md:flex md:items-start gap-5">
         <div class="md:w-1/2 lg:w-1/3 bg-white flex justify-center p-5">
-            <VueTailwindDatePicker i18n="es-es" as-single="true" no-input v-model="sales.date" :formatter="formatter" />
+            <VueTailwindDatePicker i18n="es-es" as-single no-input v-model="sales.date" :formatter="formatter" />
         </div>
         <div class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-scroll p-5 pb-32">
             <p v-if="sales.isDateSelected" class="text-center text-lg">Ventas de la fecha: <span class="font-black">{{
                 sales.date }}</span></p>
             <p v-else class="text-center text-lg">Seleccione una Fecha</p>
+
+            <div class="space-y-5">
+                <SaleDetails v-for="sale in sales.salesCollection" :key="sale.id" :sale="sale" />
+            </div>
         </div>
     </div>
 </template>
