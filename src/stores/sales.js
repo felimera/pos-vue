6314ceb3869f05acbb9b495755f889855b553b8e
andres.mera <feli041089@gmail.com>
2023-09-22 08:06:45 -0500

    Mostrando el total de ventas del día.

--- src/stores/sales.js
+++ src/stores/sales.js
@@ -20,10 +20,17 @@ export const useSalesStore = defineStore("sales", () => {
 
   const noSales = computed(() => !salesCollection.length && date.value);
 
+  const totalSalesOfDay = computed(() => {
+    return salesCollection.value
+      ? salesCollection.value.reduce((total, sale) => total + sale.total, 0)
+      : 0;
+  });
+
   return {
     date,
     isDateSelected,
     salesCollection,
     noSales,
+    totalSalesOfDay,
   };
 });
--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import VueTailwindDatePicker from 'vue-tailwind-datepicker';
 import { useSalesStore } from '../../stores/sales';
 import SaleDetails from '../../components/SaleDetails.vue';
+import { formatCurrency } from '../../helpers';
 
 const sales = useSalesStore();
 
@@ -25,6 +26,10 @@ const formatter = ref({
             <p v-else class="text-center text-lg">Seleccione una Fecha</p>
 
             <div v-if="sales.salesCollection.length" class="space-y-5">
+                <p class="text-right text-2xl">
+                    Total del día :<span class="font-black">{{ formatCurrency(sales.totalSalesOfDay) }}</span>
+                </p>
+
                 <SaleDetails v-for="sale in sales.salesCollection" :key="sale.id" :sale="sale" />
             </div>
             <p v-else-if="sales.noSales" class="text-lg text-center">No hay Ventas en este día</p>
